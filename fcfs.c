5b8b8f7ec2a6b28c3484b1d3f42c69a6099f3251
Akshaysarsamkar <115927251+Akshaysarsamkar@users.noreply.github.com>
2023-08-24 12:15:07 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,40 +1,59 @@
 #include<stdio.h>
-void findWaitingTime(int processes[],int,int bt[],int wt[])
+
+//waiting time function
+void findwaitime(int process[],int n,int bt[],int wt[])
 {
-wt[0]=0;
-for (int i =1; i<n;i++)
-wt[i]=bt[i-1] + wt[i-1] ;
+   wt[0]=0;
+   
+   for(int i=1;i<n;i++)
+     wt[i] = bt[i-1] + wt[i-1];
+  
 }
-void findTurnAroundTime(int processes[],int n,int bt[],int wt[],int tat[])
+
+
+void findtat(int process[],int n,int bt[],int wt[],int tat[])
 {
-for (int i=0; i<n ; i++)
-tat[i] =bt[i] + wt[i];
+
+  for(int i=0;i<n;i++)
+    tat[i] = bt[i] + wt[i];
+
 }
-void findavgTime(int processes[],int n,int bt[])
+
+void calavgtime(int process[],int n,int bt[])
 {
-int wt[n], tat[n],total_wt=0,total_tat=0;
-findWatingTime(process,n,bt,wt,tat);
-printf("Processes Burst time Waiting time Turn arround time\n");
-for (int i=0; i<n; i++)
-{
-total_wt =total_wt + wt[i];
-total_tat = total_tat + tat[i];
-printf("%d",(i+1));
-printf("             %d",bt[i]);
-printf("             %d",wt[i]);
-printf("             %d\n",tat[i]);
-}
-int s=(float)total_wt / (float)n;
-int t=(float)total_tat / (float)n;
-printf("Average waiting time = %d",s);
-printf("\n");
-printf("Average turn around time = %d",t);
-}
-int main()
-{
-int processes[] = {1,2,3};
-int n = sizeof processes / sizeof processes[0];
-int burst_time[] = {2,4,6};
-findavgTime(processes, n, burst_time);
-return 0;
-}
+  
+   int wt[n],tat[n],total_wt=0,total_tat=0;
+   
+   findwaitime(process,n,bt,wt);
+
+   findtat(process,n,bt,wt,tat);
+
+   printf("process brust time waiting time tat \n");
+   
+   for(int i=0;i<n;i++)
+   {
+      total_wt = total_wt + wt[i];
+      total_tat = total_tat + tat[i];
+      printf("%d\t",(i+1));
+      printf("%d\t",bt[i]);
+      printf("%d\t",wt[i]);
+      printf("%d\n",tat[i]);   
+   }
+   
+   float s =(float)total_wt / (float)n;
+   float t =(float)total_tat/ (float)n;
+   printf("avg waiting time:%f",s);
+   printf("\n");
+    printf("avg turnaround time time:%f",t);
+    }
+    
+    int main()
+    {
+          int process[] = {1,2,3,4,5,6};
+          int n = sizeof(process)/sizeof process[0];
+          
+          int brusttime[] ={9,3,2,4,3,2};
+          
+          calavgtime(process,n,brusttime);
+          return 0;
+    }
